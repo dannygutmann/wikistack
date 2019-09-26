3a41b525297b26c696b7e32b44ca6fbad09b5bcc
Danny Gutmann <dannycgutmann@gmail.com>
2019-09-26 18:03:07 -0400

    added redirect route to newly created wiki page when a user submits a create wiki page request

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const addPage = require('../views/addPage');
+const wikipage = require('../views/wikipage');
 const router = express.Router();
 const Page = require('../models/index').Page;
 
@@ -18,7 +19,7 @@ router.post('/', async (req, res, next) => {
       content,
       status,
     }).then(page => {
-      res.redirect('/');
+      res.redirect(`/wiki/${page.slug}`);
     });
   } catch (e) {
     console.log('Something happened...', e);
@@ -29,5 +30,20 @@ router.post('/', async (req, res, next) => {
 router.get('/add', (req, res) => {
   res.send(addPage());
 });
+
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    let slugReq = req.params.slug;
+    let page = await Page.findOne({
+      where: { slug: slugReq }
+    });
+
+    res.send(wikipage(page));
+  } catch(err) {
+    next(err);
+  }
+});
+
 
 module.exports = router;

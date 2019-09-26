c7db4db1433a275526500b2c1c7f4014a34c8be3
Colin Kim <ck2344@nyu.edu>
2019-09-26 11:54:09 -0400

    synced models, created schema, added validation to models

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
-const { db } = require('./models');
+const models = require('./models');
 
 const layout = require('./views/layout');
 
-db.authenticate().then(() => {
-  console.log("Connected to the database");
+models.db.authenticate().then(() => {
+  console.log('Connected to the database');
 });
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static('stylesheets'));
 
-
-app.get("/", (req, res) => {
-  res.send(layout(""));
+app.get('/', (req, res) => {
+  res.send(layout(''));
 });
 
+async function syncDB() {
+  await models.db.sync();
+}
+
+syncDB();
 
 const port = 3000;
-app.listen(port, () => console.log("Now listening on port 3000"));
+app.listen(port, () => console.log('Now listening on port 3000'));
